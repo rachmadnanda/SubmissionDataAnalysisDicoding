e9441ab1fc04d8fa406855076b5355b14d3f2d74
Rachmad Febriananda <rachmad17845@gmail.com>
2026-04-30 15:13:55 +0700

    add import os

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 # 1. Konfigurasi Halaman
 st.set_page_config(page_title="Air Quality Dashboard", page_icon="🌤️", layout="wide")
